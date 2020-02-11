5b9a4900a1503218cc22393a69235c64c9d3bb1d
Teemu Kolehmainen <teejuko@gmail.com>
2020-02-11 21:14:35 +0200

    adding jpa dependency

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,5 +20,10 @@ public class BookController {
 		List<Book> books = new ArrayList<>();
 		books.add(new Book("The Hobbit", "J. R. R. Tolkien", 1937, "978-0345339683", 6.14, new Category("0", "Scifi/Fantasia")));
 		return "List of books" + books;
+	}
+	
+	@GetMapping("/booklist")
+	public String bookList() {
+		return "booklist";
 	}
 }

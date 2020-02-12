276c475b77a1e538abf5774897606220acae1f0f
Teemu Laptop <teejuko@gmail.com>
2020-02-12 11:37:24 +0200

    major update, but editing is bugged

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,34 @@
 package com.example.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner bookstore(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			repository.save(new Book("Ensimmäinen", "author", 2011, "isbn 12312-3123", 10.0));
+			repository.save(new Book("Toinen kirja", "author", 2012, "isbn 1234-1234-23", 15.5));
+			repository.save(new Book("Kolmas kirja", "author", 2013, "isbn 123-123-123", 9.9));
+			
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+	}
 }
--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,29 +1,50 @@
 package com.example.Bookstore.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.Bookstore.domain.Book;
-import com.example.Bookstore.domain.Category;
+import com.example.Bookstore.domain.BookRepository;
 
 @Controller
-@ResponseBody
 public class BookController {
-	@RequestMapping("/index")
-	public String index() {
-		List<Book> books = new ArrayList<>();
-		books.add(new Book("The Hobbit", "J. R. R. Tolkien", 1937, "978-0345339683", 6.14, new Category("0", "Scifi/Fantasia")));
-		return "List of books" + books;
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping(value={"/", "booklist"})
+	public String Booklist(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+	@RequestMapping(value={"/add"})
+	public String addbook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
 	}
 	
-	@GetMapping("/booklist")
-	public String bookList() {
-		return "booklist";
+	@RequestMapping(value={"/edit/{id}"})
+	public String Editbook(@PathVariable("id")long bookId, Model model) {
+		model.addAttribute("book", bookId);
+		return "editbook";
+	}
+
+	@RequestMapping(value="/saveEdit", method=RequestMethod.POST)
+	public String saveEdit(Book book) {
+		repository.save(book);
+		return "redirect:../booklist";                                                                                                                                                                                     
+	}
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+	public String DeleteBook(@PathVariable("id")Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 }
